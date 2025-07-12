c1f27fb710f28886995d887baded8f192278e177
ishigame65 <moriuchi@ymail.ne.jp>
2025-07-12 10:57:03 +0900

    Add files via upload

--- js/bgcollage.js
+++ js/bgcollage.js
@@ -1,22 +1,28 @@
 
 function style_fixed100_(elem) {
-    elem.style.position = 'fixed';
+    elem.style.position = 'absolute';
     elem.style.top = '0';
     elem.style.left = '0';
     elem.style.width = '100vw';
-    elem.style.height = '100vh';
+    elem.style.height = `${document.body.clientHeight}px`;
 }
 
-function style_random_(elem, rect, szset) {
-    const px = Math.floor(Math.random() * (rect.width - 384));
-    const py = Math.floor(Math.random() * (rect.height - 384));
+function style_random_(elem, rect, szset, white_ratio) {
     const szidx = Math.floor(Math.random() * 3);
+    const sz = szset[szidx];
+    const szmax = sz * Math.sqrt(2);
+    const px = Math.floor(Math.random() * (rect.width - szmax));
+    const py = Math.floor(Math.random() * (rect.height - szmax));
     const rotang = Math.floor(Math.random() * 360);
-    elem.style.position = 'fixed';
+    elem.style.position = 'absolute';
+    elem.style.width = `${sz}px`;
+    elem.style.height = `${sz}px`;
+    elem.style.backgroundRepeat = 'no-repeat';
+    elem.style.backgroundSize = `contain`;
     elem.style.left = `${px}px`;
     elem.style.top = `${py}px`;
-    elem.style.width = `${szset[szidx]}px`;
     elem.style.transform = `rotate(${rotang}deg)`;
+    elem.style.opacity = `${1 - white_ratio}`;
 }
 
 function collage(base_num, pic_num, white_ratio) {
@@ -24,31 +30,23 @@ function collage(base_num, pic_num, white_ratio) {
     bg_collage.id = 'bg_collage';
     style_fixed100_(bg_collage);
     bg_collage.style.zIndex = '-2';
-    const bg_cover = document.createElement('div');
-    bg_cover.id = 'bg_cover';
-    style_fixed100_(bg_cover);
-    bg_cover.style.zIndex = '-1';
-    // bg_cover.style.backgroundColor = 'rgba(255,255,245,0.85)';
-    bg_cover.style.backgroundColor = `rgba(245,245,255,${white_ratio})`;
     document.body.appendChild(bg_collage);
-    document.body.appendChild(bg_cover);
-    const base_sz = [ 384, 512, 768 ];
-    const pic_sz = [ 128, 192, 256 ];
+    const base_sz = [ 512, 768, 896 ];
+    const pic_sz = [ 192, 256, 384 ];
     const bg_rect = bg_collage.getBoundingClientRect();
     for (let n = 1; n <= base_num; n++) {
-        const img = document.createElement('img');
-        img.src = `img/bliub${n}.jpg`;
+        const img = document.createElement('div');
         img.id = `b${n}`;
-        style_random_(img, bg_rect, base_sz);
+        img.style.backgroundImage = `url("img/bliub${n}.jpg")`;
+        style_random_(img, bg_rect, base_sz, white_ratio);
         img.style.filter = 'grayscale(100%)';
         bg_collage.appendChild(img);
     }
     for (let n = 1; n <= pic_num; n++) {
-        const img = document.createElement('img');
-        img.src = `img/bliuf${n}.jpg`;
+        const img = document.createElement('div');
         img.id = `f${n}`;
-        style_random_(img, bg_rect, pic_sz);
+        img.style.backgroundImage = `url("img/bliuf${n}.jpg")`;
+        style_random_(img, bg_rect, pic_sz, white_ratio);
         bg_collage.appendChild(img);
     }
 }
-// collage(6, 18, 0.85);    // base_num, pic_num
